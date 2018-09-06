1aefd5e398da1d9fd473b29a6e6e1e2ce4837548
Elias Fong <fongelias@gmail.com>
2018-09-06 14:00:42 -0400

    double-linked-list-elias

--- homework/double-linked-list.ts
+++ homework/double-linked-list.ts
@@ -2,13 +2,101 @@
 // Implement a doubly linked list
 // reversePrint()
 // Make it generic
-export default class DoublyLinkedList<T>{
+class DoublyLinkedList<T>{
 
-	public insert(value: T): void {}
+	private head: DoublyLinkedListNode<T>;
 
-	public insertAtTail(value: T): void {}
+	constructor(headValue: T) {
+		this.head = new DoublyLinkedListNode<T>(null, null, headValue);
+	}
 
-	public remove(index: number): T {}
+	public insert(value: T): void {
+		this.head = new DoublyLinkedListNode<T>(null, this.head, value);
+		if(this.head.next) { //point old head back to new head
+			this.head.next.previous = this.head;
+		}
+	}
 
-	public reversePrint(): void {}
+	public insertAtTail(value: T): void {
+		let currNode = this.head;
+		while(currNode && currNode.next) {
+			currNode = currNode.next;
+		}
+
+		currNode.next = new DoublyLinkedListNode<T>(currNode, null, value);
+	}
+
+	public remove(index: number): T { //first node is at index
+		let currNode = this.head;
+		//either bring us to the tail node or the index
+		while(currNode && currNode.next && index > 0) {
+			currNode = currNode.next;
+			index--;
+		}
+
+		if(currNode) {
+			if(currNode.previous) {
+				currNode.previous.next = currNode.next;
+			} else {
+				this.head = currNode.next;
+			}
+
+			if(currNode.next) {
+				currNode.next.previous = currNode.previous;
+			}
+		}
+
+		return currNode ? currNode.value : null;
+	}
+
+	public reversePrint(): void {
+		let currNode = this.head;
+		//either bring us to the tail node or the index
+		while(currNode && currNode.next) {
+			currNode = currNode.next;
+		}
+		//Print backwards
+		while(currNode) {
+			console.log(currNode.value);
+			currNode = currNode.previous;
+		}
+	}
 }
+
+class DoublyLinkedListNode<T> {
+	constructor(
+		public previous: DoublyLinkedListNode<T>,
+		public next: DoublyLinkedListNode<T>,
+		public value:T
+	) {}
+}
+
+
+
+const dll = new DoublyLinkedList<number>(null);
+console.log(dll.remove(1));
+console.log(dll.remove(0));
+console.log(dll.insert(1));
+console.log(dll.insert(2));
+console.log(dll.insert(3));
+console.log(dll.remove(0));
+console.log(dll.insertAtTail(0));
+console.log(dll.remove(0));
+console.log(dll.remove(0));
+console.log(dll.remove(0));
+console.log(dll.remove(0));
+console.log(dll.insert(1));
+console.log(dll.insert(2));
+console.log(dll.insert(3));
+console.log(dll.remove(1));
+console.log(dll.insert(1));
+console.log(dll.insert(2));
+console.log(dll.insert(3));
+dll.reversePrint();
+
+
+
+
+
+
+
