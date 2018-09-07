6ce9e064ead0e4f07be3d6e826b7786644096c53
Elias Fong <fongelias@gmail.com>
2018-09-07 13:31:30 -0400

    partial addition of reverse function

--- homework/double-linked-list.ts
+++ homework/double-linked-list.ts
@@ -5,9 +5,11 @@
 class DoublyLinkedList<T>{
 
 	private head: DoublyLinkedListNode<T>;
+	private directionForward: boolean;
 
 	constructor(headValue: T) {
 		this.head = new DoublyLinkedListNode<T>(null, null, headValue);
+		this.directionForward = true;
 	}
 
 	public insert(value: T): void {
@@ -24,6 +26,23 @@ class DoublyLinkedList<T>{
 		}
 
 		currNode.next = new DoublyLinkedListNode<T>(currNode, null, value);
+	}
+
+	private getLastNode(): DoublyLinkedListNode<T>{
+		let currNode = this.head;
+
+		if (this.directionForward) {
+			while(currNode && currNode.next) {
+				currNode = currNode.next;
+			}
+		} else {
+			let currNode = this.head;
+			while(currNode && currNode.previous) {
+				currNode = currNode.previous;
+			}
+		}
+		
+		return currNode;
 	}
 
 	public remove(index: number): T { //first node is at index
@@ -47,6 +66,17 @@ class DoublyLinkedList<T>{
 		}
 
 		return currNode ? currNode.value : null;
+	}
+
+	public reverse(): void {
+		console.log('rrrrr-reverse!');
+		let currNode = this.head;
+		//either bring us to the tail node or the index
+		while(currNode && currNode.next) {
+			currNode = currNode.next;
+		}
+		this.head = currNode;
+		this.directionForward = !this.directionForward;
 	}
 
 	public reversePrint(): void {
@@ -93,8 +123,10 @@ console.log(dll.insert(1));
 console.log(dll.insert(2));
 console.log(dll.insert(3));
 dll.reversePrint();
+dll.reverse();
+dll.reversePrint();
 
-
+//Reverse the ll
 
 
 
